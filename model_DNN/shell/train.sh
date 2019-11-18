a9b01e598afbf99837673d691ec384b07a0c3fb2
MengSongmiao <1301797927@qq.com>
2019-11-18 12:00:01 +0800

    None

--- model_DNN/shell/train.sh
+++ model_DNN/shell/train.sh
@@ -1,3 +1,3 @@
-python ./model_DNN/main.py --train --epochs 100 \
+python ./model_DNN/main.py --train --epochs 500 \
                            --batch_size 64 \
                            --cuda \
