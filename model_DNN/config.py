5fd8be9fd7f2d8697d7d51ed8cb966f7224f4750
MengSongmiao <1301797927@qq.com>
2019-11-18 18:14:53 +0800

    Merge branch 'master' of github.com:MSMsssss/NandFlash_GAN
    # Conflicts:
    #	data/__pycache__/__init__.cpython-37.pyc
    #	data/__pycache__/connect_database.cpython-37.pyc
    #	data/__pycache__/dataset.cpython-37.pyc
    #	model_DNN/__pycache__/__init__.cpython-37.pyc
    #	model_DNN/__pycache__/config.cpython-37.pyc

--- model_DNN/config.py
+++ model_DNN/config.py
@@ -9,5 +9,5 @@ class Config:
         self.latent_dim = 100  # 输入噪声的维度
         self.condition_dim = 1  # 输入条件的维度
         self.betas = (0.9, 0.999)  # 用于计算梯度以及梯度平方的运行平均值的系数
-        self.pe_set = [1] + list(range(100, 17000, 1000))
+        self.pe_set = [1] + list(range(100, 17000, 500))
         self.model_saved_path = os.path.dirname(os.path.abspath(__file__)) + "/save_model/"
