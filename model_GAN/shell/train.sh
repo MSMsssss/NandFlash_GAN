ce993023ee7be2477fa03d232dfc651c94d921c2
MengSongmiao <1301797927@qq.com>
2019-11-25 10:45:46 +0800

    None

--- model_GAN/shell/train.sh
+++ model_GAN/shell/train.sh
@@ -1,4 +1,4 @@
-python ./model_GAN/main.py --train --epochs 500 \
+python ./model_GAN/main.py --train --epochs 200 \
                            --batch_size 64 \
                            --cuda \
                            --err_data_name data.npy \
