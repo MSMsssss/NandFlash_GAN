7292990de6cc0fbe9b2dfdc15bf4016e05e09352
MengSongmiao <1301797927@qq.com>
2019-11-25 14:57:38 +0800

    None

--- model_GAN/shell/eval.sh
+++ model_GAN/shell/eval.sh
@@ -3,6 +3,7 @@ python ./model_GAN/main.py --eval \
                            --g_load_model_path generator_epoch_100.pth \
                            --d_load_model_path discriminator_epoch_100.pth \
                            --gen_start_pe 0 \
-                           --gen_end_pe 15000 \
-                           --gen_interval_pe 1000
+                           --gen_end_pe 17000 \
+                           --generator_data_num 100 \
+                           --gen_interval_pe 500
 
