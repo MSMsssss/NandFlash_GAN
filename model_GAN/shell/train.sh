8a0e0d4fea8c8bc36ba989f950b71d55a1a6df74
MengSongmiao <1301797927@qq.com>
2019-11-25 10:46:25 +0800

    None

--- model_GAN/shell/train.sh
+++ model_GAN/shell/train.sh
@@ -3,5 +3,5 @@ python ./model_GAN/main.py --train --epochs 200 \
                            --cuda \
                            --err_data_name data.npy \
                            --condition_data_name condition.npy \
-                           --save_model_epoch 100
+                           --save_model_epoch 20
 
