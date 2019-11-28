b0b45b296bbc6713b0218c026a8d10287cbdf769
MengSongmiao <1301797927@qq.com>
2019-11-28 14:39:32 +0800

    None

--- model_cGAN/shell/train.py
+++ model_cGAN/shell/train.py
@@ -1,8 +1,11 @@
-python ./model_cGAN/main.py --train \
+import os
+command = "python ./model_cGAN/main.py --train \
                            --cuda \
                            --latent_dim 40 \
                            --epochs 500 \
                            --batch_size 64 \
                            --err_data_name data.npy \
                            --condition_data_name condition.npy \
-                           --save_model_epoch 100 \
+                           --save_model_epoch 100"
+
+os.system(command)
