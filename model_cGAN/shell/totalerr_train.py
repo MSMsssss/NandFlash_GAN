04b58786b22d239e7c89ce0671a477e1ee9b7288
MengSongmiao <1301797927@qq.com>
2019-12-03 16:36:59 +0800

    Merge branch 'master' of github.com:MSMsssss/NandFlash_GAN
    # Conflicts:
    #	data/__pycache__/__init__.cpython-37.pyc
    #	data/__pycache__/connect_database.cpython-37.pyc
    #	data/__pycache__/dataset.cpython-37.pyc
    #	model_DNN/__pycache__/__init__.cpython-37.pyc
    #	model_DNN/__pycache__/config.cpython-37.pyc

--- model_cGAN/shell/totalerr_train.py
+++ model_cGAN/shell/totalerr_train.py
@@ -1,7 +1,7 @@
 import os
 command = "python ./model_cGAN/total_err_gen.py --train \
                            --cuda \
-                           --latent_dim 10 \
+                           --latent_dim 20 \
                            --epochs 100 \
                            --batch_size 64 \
                            --err_data_name data.npy \
