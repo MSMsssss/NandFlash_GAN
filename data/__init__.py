d97333d0617bef48e123a1fb3a553caf7b360d67
msm <1301797927@qq.com>
2020-03-20 17:50:30 +0800

    None

--- data/__init__.py
+++
@@ -1,9 +0,0 @@
-result = np.load(data_root_path + file_prefix + "_" + file_list[0] + ".npy").astype(np.float32)
-print("%s load success" % file_list[0])
-file_list.remove(file_list[0])
-
-for file in file_list:
-    result = np.concatenate((result,
-                             np.load(data_root_path + file_prefix + "_" + file + ".npy").astype(np.float32)),
-                            axis=0)
-    print("%s load success" % file)
